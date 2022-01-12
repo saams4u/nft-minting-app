df035de546bcbbe044c31d413e42922df61d28c1
saams4u <mahjouri.saamahn@gmail.com>
2022-01-12 15:41:12 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -310,10 +310,7 @@ function App() {
               >
                 Rarible
               </StyledButton><br></br>
-              <a href="https://twitter.com/saams4u?ref_src=twsrc%5Etfw" 
-              class="twitter-follow-button" data-show-count="false">Follow @saams4u</a>
-                <script async src="https://platform.twitter.com/widgets.js" 
-                charset="utf-8"></script>
+              <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
             </span>
             <s.SpacerSmall />
             {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
