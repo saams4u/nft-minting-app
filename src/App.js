448898db6110228e83ecc2dc198f1972f6990bf3
saams4u <mahjouri.saamahn@gmail.com>
2022-01-24 23:04:51 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -344,15 +344,14 @@ function App() {
               >
                 Reddit
               </StyledButton>  
-              <br></br><br></br><br></br><br></br>
               <StyledButton
                 style={{
                   backgroundImage: "url('https://pbs.twimg.com/profile_images/1370144254694199306/cG08lUcQ_400x400.jpg')",
                   backgroundPosition: 'center',
                   backgroundSize: 'cover',
                   backgroundRepeat: 'no-repeat',
-                  height: 100,
-                  width: 100,              
+                  height: 50,
+                  width: 80              
                 }}
                 onClick={(e) => {
                   window.open("https://nftcalendar.io/event/guild-fighters/", "_blank");
